2a0aa126e6692d3e24535a206c4091591f235e98
deepak <deepaksood619>
2016-04-04 00:21:04 +0530

    Initial commit with directory structure

---
+++ app/src/main/java/deepaksood/in/pcsmaassignment4/AddMobileActivity.java
@@ -0,0 +1,13 @@
+package deepaksood.in.pcsmaassignment4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMobileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mobile);
+    }
+}
